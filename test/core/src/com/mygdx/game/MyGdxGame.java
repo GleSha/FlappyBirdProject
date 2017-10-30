fc0395a6a92971d223d4f9381197e89dbd16b03f
GleSha <gleb.shaforostov@yandex.ru>
2017-10-30 21:41:54 +0700

    Фактически пустой проект, где еще ничего нет, кроме окна и перемещающейся картинки.

---
+++ test/core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,47 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	Vector2 pos;
+	int Speed;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		pos = new Vector2(100, 40);
+		Speed = 1;
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0.08f, 0.75f, 0.77f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, pos.x, pos.y);
+		batch.end();
+		update();
+	}
+
+	public void update() {
+		pos.x += Speed;
+		if (pos.x == 500)
+			Speed = -1;
+		if (pos.x == 100)
+			Speed = 1;
+	}
+
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
